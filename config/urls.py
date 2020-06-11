39d462325e8ef57905f3a059ea3aa1ce94ff96eb
Jeff <Salisbury.jeffery@gmail.com>
2020-06-10 20:53:22 -0400

    Adding DRF and setting up a root endpoint view for starting up localhost.

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class EndpointsView(APIView):
+    """API Endpoints view on root """
+
+    def get(self, request, *args, **kwargs):
+        apidocs = {
+            "Login": request.build_absolute_uri('api-auth/login/'),
+            "Admin": request.build_absolute_uri('admin/')
+        }
+        return Response(apidocs)
+
 
 urlpatterns = [
+    path('', EndpointsView.as_view()),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
