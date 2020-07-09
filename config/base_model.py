ff1e271b5c53289d75a065892b326635ae08a759
Jeff <salisbury.jeffery@gmail.com>
2020-07-09 11:06:00 -0400

    Base class to inherit in class models to use snaketocamel cameltosnake transformation.

---
+++ config/base_model.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from .case.mixins import FromCamelCase, ToCamelCase
+
+
+class BaseClass(models.Model, FromCamelCase, ToCamelCase):
+    active = models.BooleanField(default='True')
+    created_at = models.DateField(auto_now_add=True)
+    created_by = models.CharField(max_length=255, default='')
+    updated_at = models.DateField(auto_now=True)
